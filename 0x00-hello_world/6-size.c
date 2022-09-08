956c75a2099b4abdcc690c562767970aaced4a30
thyrox02 <lukmanafolayan@gmail.com>
2022-09-08 13:21:19 -0700

    wahala

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ * Return: Always 0 (Success)
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
@@ -26,4 +22,3 @@ printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 return (0);
 }
-
