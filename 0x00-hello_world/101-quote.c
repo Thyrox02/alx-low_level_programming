bbe60b9700cff248258c8e777af67265a8304ee5
thyrox02 <lukmanafolayan@gmail.com>
2022-09-08 15:29:14 -0700

    genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 /**
  * main - Entry point
  *
- * return: Always 1 (Success)
- * /
- * int main (void)
+ * Return: Always 1 (Success)
+ */
+int main(void)
 {
-Char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(2, str1, 59);
-return (1)
+	write(2, str1, 59);
+	return (1);
 }
-
